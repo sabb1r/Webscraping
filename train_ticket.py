4b19b3a9935ce6ce9380186456380a738d56ede5
Sabbir Ahmed <sabbirahmed.engr@gmail.com>
2023-09-11 01:09:53 +0600

    Update

--- train_ticket.py
+++ train_ticket.py
@@ -18,16 +18,5 @@ train_div = soup.find('h2', string='BANALATA EXPRESS (792)').parent.parent
 
 seat_category_div = train_div.find(has_seat_type)
 
-# seat_category_div = train_div.find('div', class_='seat-classes-row d-flex').find('span', string='SNIGHDA').parent.parent
-# print(seat_category_div.prettify())
 print('Available seats = ', seat_category_div.find('span', class_='all-seats').string)
-# seat_tag = seat_type_div.find('span', class_='all-seats')
-# print(seat_tag.string)
-# print(train_div.children)
 
-# for child in train_div.descendants:
-#     if child.string == 'SNIGDHA':
-#         available_seat_tag = child.parent.sibling
-#         break
-# print(available_seat_tag.prettify())
-# print(train_div.prettify())
