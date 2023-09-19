ada22ba437abdd0ffc3bd6a3abd2d3753144857d
sabbir <sabbirahmed.engr@gmail.com>
2023-09-19 15:46:31 +0600

    Update

--- get_the_html.py
+++ get_the_html.py
@@ -1,14 +1,53 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
-chrome_options = Options()
-chrome_options.add_argument("--headless")
-driver = webdriver.Safari(options=chrome_options)
-url = 'https://eticket.railway.gov.bd/booking/train/search/en?fromcity=Rajshahi&tocity=Dhaka&doj=13-Sep-2023&class=SNIGDHA'
+month_name = {
+    '01': 'Jan',
+    '02': 'Feb',
+    '03': 'Mar',
+    '04': 'Apr',
+    '05': 'May',
+    '06': 'Jun',
+    '07': 'Jul',
+    '08': 'Aug',
+    '09': 'Sep',
+    '10': 'Oct',
+    '11': 'Nov',
+    '12': 'Dec'
+}
 
-driver.get(url)
 
-with open('response', 'w') as f:
-    f.write(driver.page_source)
+def get_url(from_station, to_station, journey_date, seat_type):
+    if not from_station.isalpha():
+        from_station = '%20'.join(from_station.split(' '))
+    if not to_station.isalpha():
+        to_station = '%20'.join(to_station.split(' '))
 
-driver.quit()
+    year, month, date = journey_date.split('/')
+    journey_date = date + '-' + month_name[month] + '-' + year
+
+    return 'https://eticket.railway.gov.bd/booking/train/search/en?fromcity={}&tocity={}&doj={}&class={}'.format(
+        from_station, to_station, journey_date, seat_type)
+
+
+def get_html(url):
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    driver = webdriver.Chrome(options=chrome_options)
+    driver.get(url)
+
+    with open('response', 'w', encoding='utf-8') as f:
+        f.write(driver.page_source)
+
+    driver.quit()
+
+
+from_city = input('Enter the from-city: ')
+to_city = input('Enter the to-city: ')
+doj = input('Enter date of journey <YYYY/MM/DD>: ')
+coach = input('Enter the coach type: ')
+no_ticket = int(input('Enter how many tickets you want to purchase: '))
+
+url = get_url(from_city, to_city, doj, coach)
+print(url)
+get_html(url)
--- train_ticket.py
+++ train_ticket.py
@@ -1,74 +1,30 @@
 # import datetime
 from bs4 import BeautifulSoup
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 
-chrome_options = Options()
-# chrome_options.add_argument("--headless")
-# driver = webdriver.Chrome(options=chrome_options)
-driver = webdriver.Chrome()
+with open('response', 'r', encoding='utf-8') as f:
+    soup = BeautifulSoup(f, 'html5lib')
 
-day_name = ('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday')
-month_name = {
-    '01': 'Jan',
-    '02': 'Feb',
-    '03': 'Mar',
-    '04': 'Apr',
-    '05': 'May',
-    '06': 'Jun',
-    '07': 'Jul',
-    '08': 'Aug',
-    '09': 'Sep',
-    '10': 'Oct',
-    '11': 'Nov',
-    '12': 'Dec'
-}
+train_div = {val.find('h2').string: val for val in soup.find_all('div', class_='row single-trip-wrapper list_rows')}
 
 
-def get_html(url):
-    driver.get(url)
-    with open('response', 'w', encoding='utf_8') as f:
-        f.write(driver.page_source)
+if not train_div:
+    print('Sorry no train is available')
+else:
+    print('The following trains are available in that day:- ')
+    train_list = list(train_div.keys())
+    for serial, train in enumerate(train_list):
+        print('{}| {}'.format(serial+1, train))
 
-    driver.quit()
+    choice = input('Do you want to board any specific train? <Type "Y" for YES and "N" for "NO> ')
+    if choice == 'Y':
+        particular_train = train_list[int(input('Enter the train: <Type the train number serial in the display> ')) - 1]
+    else:
+        particular_train = None
 
-
-def get_url(from_station, to_station, journey_date, seat_type):
-    if not from_station.isalpha():
-        from_station = '%20'.join(from_station.split(' '))
-    if not to_station.isalpha():
-        to_station = '%20'.join(to_station.split(' '))
-
-    year, month, date = journey_date.split('/')
-    journey_date = date + '-' + month_name[month] + '-' + year
-
-    return 'https://eticket.railway.gov.bd/booking/train/search/en?fromcity={}&tocity={}&doj={}3&class={}'.format(from_station, to_station, journey_date, seat_type)
+    print(particular_train)
 
 
-# def get_available_train(starting_station, ending_station, date):
-#     date_tup = tuple(map(int, date.split('/')))
-#     date_obj = datetime.date(*date_tup)
-#     day = day_name[date_obj.weekday()]
 
-
-from_city = input('Enter the from-city: ')
-to_city = input('Enter the to-city: ')
-doj = input('Enter date of journey <YYYY/MM/DD>: ')
-coach = input('Enter the coach type: ')
-no_ticket = int(input('Enter how many tickets you want to purchase: '))
-
-# train_name = input('Enter the ')  <- Provide user to enter a specific train. Have to improvise later.
-
-url = get_url(from_city, to_city, doj, coach)
-
-print(url)
-
-get_html(url)
-
-# with open('response.html', 'r') as f:
-#     soup = BeautifulSoup(f, 'html5lib')
-#
-#
 # def has_seat_type(tag):
 #     if tag.has_attr('data-seat-type'):
 #         if 'AC_S' in tag['data-seat-type']:
